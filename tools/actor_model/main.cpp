7fc9cb98170f203253ce47e67350b88d4f42c28c
Roomba, disciple of machine code <107208596+isofinly@users.noreply.github.com>
2025-05-26 12:44:07 +0300

    Update main.cpp

--- tools/actor_model/main.cpp
+++ tools/actor_model/main.cpp
@@ -3,34 +3,47 @@
 #include <library/cpp/actors/core/scheduler_basic.h>
 #include <util/generic/xrange.h>
 
-THolder<NActors::TActorSystemSetup> BuildActorSystemSetup(ui32 threads, ui32 pools) {
+namespace {
+    constexpr ui32 DEFAULT_THREADS = 20;
+    constexpr ui32 DEFAULT_POOLS = 1;
+    constexpr ui32 EXECUTOR_POOL_SIZE = 512;
+    constexpr ui32 SCHEDULER_POOL_SIZE = 512;
+    constexpr auto MAIN_LOOP_SLEEP_INTERVAL = TDuration::MilliSeconds(200);
+    constexpr auto SELF_PING_INTERVAL = TDuration::Seconds(1);
+}
+
+THolder<NActors::TActorSystemSetup> BuildActorSystemSetup(ui32 threads = DEFAULT_THREADS, ui32 pools = DEFAULT_POOLS) {
     auto setup = MakeHolder<NActors::TActorSystemSetup>();
     setup->ExecutorsCount = pools;
     setup->Executors.Reset(new TAutoPtr<NActors::IExecutorPool>[pools]);
+    
     for (ui32 idx : xrange(pools)) {
-        setup->Executors[idx] = new NActors::TBasicExecutorPool(idx, threads, 512);
+        setup->Executors[idx] = new NActors::TBasicExecutorPool(idx, threads, EXECUTOR_POOL_SIZE);
     }
-    setup->Scheduler.Reset(new NActors::TBasicSchedulerThread(NActors::TSchedulerConfig(512, 0)));
+    
+    setup->Scheduler.Reset(new NActors::TBasicSchedulerThread(
+        NActors::TSchedulerConfig(SCHEDULER_POOL_SIZE, 0)));
     return setup;
 }
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
     Y_UNUSED(argc, argv);
-    auto actorySystemSetup = BuildActorSystemSetup(20, 1);
-    NActors::TActorSystem actorSystem(actorySystemSetup);
+    
+    auto actorSystemSetup = BuildActorSystemSetup();
+    NActors::TActorSystem actorSystem(actorSystemSetup);
     actorSystem.Start();
 
-    actorSystem.Register(CreateSelfPingActor(TDuration::Seconds(1)).Release());
+    actorSystem.Register(CreateSelfPingActor(SELF_PING_INTERVAL).Release());
 
-    // Зарегистрируйте Write и Read акторы здесь
+    auto writeActorId = actorSystem.Register(CreateWriteActor().Release());
+    actorSystem.Register(CreateReadActor(std::cin, writeActorId).Release());
 
-    // Раскомментируйте этот код
-    // auto shouldContinue = GetProgramShouldContinue();
-    // while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
-    //     Sleep(TDuration::MilliSeconds(200));
-    // }
+    auto shouldContinue = GetProgramShouldContinue();
+    while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
+        Sleep(MAIN_LOOP_SLEEP_INTERVAL);
+    }
+    
     actorSystem.Stop();
     actorSystem.Cleanup();
-    // return shouldContinue->GetReturnCode();
+    return shouldContinue->GetReturnCode();
 }
